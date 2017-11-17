5492ee13645c8b0546e9ad618277e11dde150890
zhangzhiya <zhangzhiya@mail.bcl.com.cn>
2017-11-17 17:30:07 +0800

    bbb

---
+++ Library_3_IMG_Glide_3.7.0/src/cn/com/zzy/GlideActivity.java
@@ -0,0 +1,15 @@
+package cn.com.zzy;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import cn.com.zzy.glide.R;
+
+public class GlideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_glide);
+    }
+}
